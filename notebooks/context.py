de43b6c11c88919730add3443900e596930d1830
AndrewSpiro <s4015355@vuw.leidenuniv.nl>
2026-04-14 13:55:03 +0200

    updated context.py

--- notebooks/context.py
+++ notebooks/context.py
@@ -3,3 +3,6 @@ import sys
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src/')))
 
 import LocalLearning
+
+# ## adding data as path for repro models acs
+# sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../data/')))
