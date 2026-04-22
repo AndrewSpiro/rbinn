a309253e7ccb89fbf75cb340e1cfa6696800e084
AndrewSpiro <s4015355@vuw.leidenuniv.nl>
2026-04-22 14:29:56 +0200

    added comments for sources of results

--- binn3_eat/compute_stats.py
+++ binn3_eat/compute_stats.py
@@ -1,9 +1,9 @@
 import numpy as np
 
 accuracies = [
-    [85.5, 62.2],
-    [74.7, 53.5],
-    [74.22, 54.3]
+    [85.5, 62.2], # binn3_eat/Rescifar10_backup/FGSM/results_rgbedge.txt lines 5 &6
+    [74.7, 53.5], # binn3_eat/Rescifar10/FGSM/results_rgbedge_backup1.txt lines 5&6
+    [74.22, 54.3] # binn3_eat/Rescifar10/FGSM/results_rgbedge.txt lines 13&14
 ]
 
 means = np.mean(accuracies, axis = 0)
