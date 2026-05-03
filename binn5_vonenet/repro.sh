4557a6d2185525c6e405746fa1222be844d395bb
AndrewSpiro <s4015355@vuw.leidenuniv.nl>
2026-05-03 10:16:51 +0200

    reorganized and corrected train call

--- binn5_vonenet/repro.sh
+++ binn5_vonenet/repro.sh
@@ -11,12 +11,13 @@ RUN_EVAL=true
 MODEL_ARCH=resnet50
 
 if [ "$DEBUG" = true ]; then
-    EPOCHS=2
+    EPOCHS=1
     TRAIN_SEEDS=(0)
     ATTACK_SEEDS=(100)
     OUTPUT_BASE=output_debug
     RESTORE_EPOCH=1
     RESTORE_BASE=restore_debug
+    RESULTS_DIR=debug_results
     echo "Running in debug mode..."
 else
     EPOCHS=70
@@ -25,6 +26,7 @@ else
     OUTPUT_BASE=output
     RESTORE_EPOCH=5
     RESTORE_BASE=restore
+    RESULTS_DIR=results
 fi
 
 for T_SEED in "${TRAIN_SEEDS[@]}"
@@ -34,32 +36,26 @@ do
         echo "Running training with seed $T_SEED"
 
         mkdir -p $OUTPUT_BASE
+        VONENET_DIR="${OUTPUT_BASE}/${MODEL_ARCH}_vonenet_seed_${T_SEED}"
+        
         mkdir -p $RESTORE_BASE
+        
+        mkdir -p "${RESULTS_DIR}/${MODEL_ARCH}_seed_${T_SEED}"
+        RESULTS_PATH="${RESULTS_DIR}/${MODEL_ARCH}_seed_${T_SEED}/results.json"
 
-        python train.py \
+        python train.py train \
         --dataset cifar10 \
-        --in_path ./data \
+        --in_path ../data \
         --ngpus 1 \
         --epochs $EPOCHS \
         --batch_size 64 \
         -o "${OUTPUT_BASE}/${MODEL_ARCH}_vonenet_seed_${T_SEED}" \
-        -restore_epoch $RESTORE_EPOCH \
         -restore_path "$RESTORE_BASE/${MODEL_ARCH}_vonenet_seed_${T_SEED}" \
-        --model_arch $MODEL_ARCH
+        --model_arch $MODEL_ARCH \
+        --vonenet_dir $VONENET_DIR
 
         echo 'Finished training.'
     fi
-
-    if [ "$DEBUG" = true ]; then
-        RESULTS_DIR=debug_results
-        VONENET_DIR="${OUTPUT_BASE}/${MODEL_ARCH}_vonenet_seed_${T_SEED}"
-    else
-        RESULTS_DIR=results
-        VONENT_DIR="${OUTPUT_BASE}/${MODEL_ARCH}_vonenet_seed_${T_SEED}"
-    fi
-
-    mkdir -p "${RESULTS_DIR}/${MODEL_ARCH}_seed_${T_SEED}"
-    RESULTS_PATH="${RESULTS_DIR}/${MODEL_ARCH}_seed_${T_SEED}/results.json"
 
     if [ "$RUN_EVAL" = true ]; then
         python run.py \
