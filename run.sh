780476defea5d0bb01345b1cb196da8e1b102700
AndrewSpiro <s4015355@vuw.leidenuniv.nl>
2026-05-05 14:39:39 +0200

    added some variables; validation calls partially; rd call

--- run.sh
+++ run.sh
@@ -2,9 +2,34 @@
 
 set -e
 
+source $(conda info --base)/etc/profile.d/conda.sh
+
+DEBUG=true
+RUN_VALIDS=false
+
+MODELS=("pixelreg" "eat")
+EPSILON_SPACE=berger
+PGD_NUM_ITER=40
+PGD_STEP_SIZE=0.01
+
 echo "Starting full pipeline..."
 
-echo "Starting BINN1 pipeline..."
-bash binn1_pixelreg/repro.sh
+if [ "$RUN_VALIDS" = true ]; then
+    echo "Starting validations..."
+    echo "Starting BINN1 pipeline..."
+    bash binn1_pixelreg/repro.sh
+else
+    echo "Skipping validations"
+fi
+
+conda activate verona_env
+for m in "${MODELS[@]}"; do
+    echo "Obtaining robustness distribution for $m"
+    python create_robustness_dist.py $m \
+        --epsilon_space $EPSILON_SPACE \
+        --bool_debug $DEBUG \
+        --pgd_num_iter $PGD_NUM_ITER \
+        --pgd_step_size $PGD_STEP_SIZE
+done
 
 echo "Completed all experiments successfully"
