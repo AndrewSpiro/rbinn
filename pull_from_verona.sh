4015edc23a6f60333d9937da872f46f39dbd1f1a
AndrewSpiro <s4015355@vuw.leidenuniv.nl>
2026-05-05 15:17:35 +0200

    added allow unrelated history and squash and commit msg

--- pull_from_verona.sh
+++ pull_from_verona.sh
@@ -1,2 +1,3 @@
 git fetch verona
-git merge -s subtree verona/main
+git merge --squash -s subtree verona/main --allow-unrelated-histories
+git commit -m "Update VERONA subtree to latest remote version"
