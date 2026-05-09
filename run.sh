e07f41359833e2f20e25fa777a2650fb9184d606
AndrewSpiro <s4015355@vuw.leidenuniv.nl>
2026-05-09 18:08:43 +0200

    select model based on adv or clean; aggregating optional

--- run.sh
+++ run.sh
@@ -6,7 +6,8 @@ source $(conda info --base)/etc/profile.d/conda.sh
 
 DEBUG=true
 RUN_VALIDS=false
-GET_RDS=false
+GET_RDS=true
+AGG_RESULTS=false
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 DATA_DIR="${SCRIPT_DIR}/data"
@@ -15,7 +16,7 @@ mkdir -p "$DATA_DIR"
 EXP_REPO_PATH="${SCRIPT_DIR}/experiments"
 
 declare -A MODEL_IDS=( ["pixelreg"]=1 ["eat"]=3 ["cnnf"]=4 ["vonenet"]=5)
-MODELS=("pixelreg")
+MODELS=("eat")
 EPSILON_SPACE=berger
 PGD_NUM_ITER=40
 PGD_STEP_SIZE=0.01
@@ -51,19 +52,23 @@ if [ "${GET_RDS}" = true ]; then
             --pgd_num_iter $PGD_NUM_ITER \
             --pgd_step_size $PGD_STEP_SIZE \
             --data_dir $DATA_DIR \
-            --exp_repo_path "${EXP_REPO_PATH}"
+            --exp_repo_path "${EXP_REPO_PATH}" \
+            --model_seed 0 \
+            --train_type clean
     done
 fi
 
-echo "#### Aggregating results. Please manually update your experiments json with the desired experiments to include. ####"
+if [ "${AGG_RESULTS}" = true ]; then
+    echo "#### Aggregating results. Please manually update your experiments json with the desired experiments to include. ####"
 
-if [ "${DEBUG}" = true ]; then
-    RESULTS_DIR=${SCRIPT_DIR}/debug_rd_results
-else
-    RESULTS_DIR=${SCRIPT_DIR}/rd_results
+    if [ "${DEBUG}" = true ]; then
+        RESULTS_DIR=${SCRIPT_DIR}/debug_rd_results
+    else
+        RESULTS_DIR=${SCRIPT_DIR}/rd_results
+    fi
+    mkdir -p $RESULTS_DIR
+
+    python rd_analysis.py --experiments_path experiments.json --results_dir "${RESULTS_DIR}" 
 fi
-mkdir -p $RESULTS_DIR
-
-python rd_analysis.py --experiments_path experiments.json --results_dir "${RESULTS_DIR}" 
 
 echo "Completed all experiments successfully"
