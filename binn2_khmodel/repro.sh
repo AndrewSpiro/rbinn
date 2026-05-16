0b2c3dcc99eb0ae80cd366084bc5e3ce2ae09434
AndrewSpiro <s4015355@vuw.leidenuniv.nl>
2026-05-16 17:28:24 +0200

    outline of repro

---
+++ binn2_khmodel/repro.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+set -e
+
+source $(conda info --base)/etc/profile.d/conda.sh
+
+conda activate khmodel
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+ROOT_DIR="$(dirname "$SCRIPT_DIR")"
+export PYTHONPATH="${ROOT_DIR}:${PYTHONPATH}"
+
+DEBUG=true
+RUN_TRAINING=true
+RUN_ATTACKS=true
+
+if [ "$DEBUG" = true ]; then
+    TRAIN_SEEDS=(0)
+    ATTACK_SEEDS=(101)
+    echo "--- RUNNING IN DEBUG MODE ---"
+else
+    TRAIN_SEEDS=(0 1 2)
+    ATTACK_SEEDS=(100 101 102)
+fi
+
+for T_SEED in "${TRAIN_SEEDS[@]}"
+do
+    if [ "$RUN_TRAINING" = true ]; then
+        echo "Starting training for train seed $T_SEED"
+    fi
+    if [ "$RUN_ATTACKS" = true ]; then
+        echo "Starting attacks for attack seeds $ATTACK_SEEDS"
+        for A_SEED in "${ATTACK_SEEDS[@]}"
+        do
+            echo "Running attack with seed $A_SEED on model $T_SEED"
+        done
+    fi
+done
