803313a1ea2b3218d69bf71d6320363c9ae6b407
AndrewSpiro <s4015355@vuw.leidenuniv.nl>
2026-05-20 14:24:02 +0200

    partial progress

--- binn2_khmodel/src/run_attacks.py
+++ binn2_khmodel/src/run_attacks.py
@@ -4,8 +4,6 @@ import argparse
 
 import torch
 from torchvision.transforms import ToTensor
-from torch import Tensor
-
 
 from binn2_khmodel.tests.context import *
 from LocalLearning import ModelFactory, HiddenLayerModel, KHModel, SHLP, FKHL3
@@ -31,18 +29,27 @@ def str2bool(v):
         return False
     else:
         raise argparse.ArgumentTypeError('Boolean value expected.')
-
-class IdentityModel(HiddenLayerModel):
-    def __init__(self):
-        super().__init__()
-        self.flat = torch.nn.Flatten(start_dim=1)
-        
-    def hidden(self, x: Tensor) -> Tensor:
-        return self.flat(x)
     
-    def _forward(self, x: Tensor) -> Tensor:
-        h = self.hidden(x)
-        return (h, h)
+def saveKHModelPerturbationResults(exp: PerturbationExperiment, attack: str):
+    '''making this function to save epsilon values and accuracies -aspiro'''
+    crit_norm = []
+    for i, (key, results) in enumerate(exp):
+        if key != 'khmodel_cifar10_pruned.pty': # KH is the target model to reproduce
+            continue
+        else:
+            cns_nan = np.isnan(results['crit_eps'])
+            crit_norm.append(results['crit_norm'][~cns_nan])
+            with open(f"{attack}_attack.txt", "w") as f:
+                f.write("Epsilons\n")
+                for eps in results['eps']:
+                    f.write(f"{eps}\n")
+                f.write("\nCritical Epsilons\n")
+                for crit_eps in crit_norm[0]:
+                    f.write(f"{crit_eps}\n")
+                f.write("\nAccuracies\n")
+                for acc in results['acc']:
+                    f.write(f"{acc}\n")
+            print("Saved adv attack results")
 
 if __name__ == "__main__":
 
@@ -51,26 +58,25 @@ if __name__ == "__main__":
     parser.add_argument('--attack_seed', type=int, help='seed for attacking')
     parser.add_argument('--data_path', type=str, help="path to the data e.g., 'root/data'")
     parser.add_argument('--model_path', type=str, help="path for the model e.g., 'root/binn2_khmodel/data/repro/models")
+    parser.add_argument('--figure_path', type=str, help="path for figures e.g., 'root/data/repro/figures'")
+    parser.add_argument('--exp_path', type=str, help="path for the epxeriments e.g., 'root/binn2_khmodel/data/repro/experiments")
     parser.add_argument('--khlayer_name', type=str, default='fkhl3_cifar10_pruned.pty', help='name of the layer')
-    parser.add_argument('--attack_models', choice=['khmodel', 'shlp', 'L2', 'jreg', 'specreg'], help="models to train")
+    parser.add_argument('--attack_models', choices=['khmodel', 'shlp', 'L2', 'jreg', 'specreg'], help="models to train")
+    parser.add_argument('--train_seed', type=int, help='seed for training')
 
 
     args = parser.parse_args()
-
-    ROOT = Path(__file__).resolve().parent.parent
-    if args.debug:
-        ROOT = ROOT/"debug"
 
     # create directory structure
     model_path = Path(args.model_path)
     if not os.path.exists(model_path):
         os.makedirs(model_path)
 
-    figure_path = ROOT / "data/repro/figures"
+    figure_path = Path(args.figure_path)
     if not os.path.exists(figure_path):
         os.makedirs(figure_path)
 
-    exp_path = ROOT / "data/repro/experiments"
+    exp_path = Path(args.exp_path)
     if not os.path.exists(exp_path):
         os.makedirs(exp_path)
 
@@ -92,6 +98,11 @@ if __name__ == "__main__":
 
     fn_list = [fn for model in args.attack_models for fn in fn_dict[model]]
 
+    rp_fname = Path("random_perturbation_results.pkl")
+    fgsm_fname = Path("fgsm_results.pkl")
+    pgd_fname = Path("pgd_results.pkl")
+
+    # hyper parameters
     BATCH_SIZE = 1000
 
     if torch.cuda.is_available():
@@ -99,45 +110,33 @@ if __name__ == "__main__":
     else:
         device = torch.device('cpu')
 
+    print(f"args data path: {args.data_path}")
     cifar10Test = LpUnitCIFAR10(
     root=args.data_path,
     train=False,
     transform=ToTensor(),
     p=FKHL3.pSet["p"],
     )
-
-    idmodel = IdentityModel()
-
-    TestLoader = DeviceDataLoader(
-                cifar10Test,
-                device=device,
-                batch_size=BATCH_SIZE,
-                num_workers=4,
-                shuffle=False,
-            )
-
-    rp_fname = Path("random_perturbation_results.pkl")
-    fgsm_fname = Path("fgsm_results.pkl")
-    pgd_fname = Path("pgd_results.pkl")
-
-
-    print(f"args data path: {args.data_path}")
     
     ce_loss = torch.nn.CrossEntropyLoss()
     eps = np.logspace(-6, np.log(2.5), num=400)
 
     rpE = RandomPerturbationExperiment(ce_loss)
-    rpE.run(model_path, [model_name], cifar10Test, eps, device, norm_p=2.0)
+    rpE.run(model_path, fn_list, cifar10Test, eps, device, norm_p=2.0)
     rpE.save(exp_path / rp_fname)
 
     breakpoint()
 
     fgsmE = FGSMExperiment(ce_loss)
-    # fgsmE.run(directory, fn_list, cifar10Test, eps, device, norm_p=2.0) #acs
-    # fgsmE.save(exp_directory / fgsm_fname) # acs
-    fgsmE.load(exp_directory / fgsm_fname)
+    fgsmE.run(model_path, fn_list, cifar10Test, eps, device, norm_p=2.0)
+    fgsmE.save(exp_path / fgsm_fname)
+    fgsmE.load(exp_path / fgsm_fname)
 
     pgdE = PGDExperiment(ce_loss)
-    # pgdE.run(directory, fn_list, cifar10Test, eps, device, norm_p=2.0) #acs
-    # pgdE.save(exp_directory / pgd_fname) #acs
-    pgdE.load(exp_directory / pgd_fname)
+    pgdE.run(model_path, fn_list, cifar10Test, eps, device, norm_p=2.0)
+    pgdE.save(exp_path / pgd_fname)
+    pgdE.load(exp_path / pgd_fname)
+    
+
+    clrs = ['c0','c1','c2','c3','c4','c5']
+    names = ['KH', 'BP', 'L2', 'JReg', 'SpecReg']
