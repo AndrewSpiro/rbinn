5a86c9504526e4ee09a491da01c1267bb5fc2610
AndrewSpiro <s4015355@vuw.leidenuniv.nl>
2026-05-20 12:02:50 +0200

    partial updates

--- binn2_khmodel/src/run_attacks.py
+++ binn2_khmodel/src/run_attacks.py
@@ -4,6 +4,8 @@ import argparse
 
 import torch
 from torchvision.transforms import ToTensor
+from torch import Tensor
+
 
 from binn2_khmodel.tests.context import *
 from LocalLearning import ModelFactory, HiddenLayerModel, KHModel, SHLP, FKHL3
@@ -30,6 +32,18 @@ def str2bool(v):
     else:
         raise argparse.ArgumentTypeError('Boolean value expected.')
 
+class IdentityModel(HiddenLayerModel):
+    def __init__(self):
+        super().__init__()
+        self.flat = torch.nn.Flatten(start_dim=1)
+        
+    def hidden(self, x: Tensor) -> Tensor:
+        return self.flat(x)
+    
+    def _forward(self, x: Tensor) -> Tensor:
+        h = self.hidden(x)
+        return (h, h)
+
 if __name__ == "__main__":
 
     parser=argparse.ArgumentParser()
@@ -38,6 +52,8 @@ if __name__ == "__main__":
     parser.add_argument('--data_path', type=str, help="path to the data e.g., 'root/data'")
     parser.add_argument('--model_path', type=str, help="path for the model e.g., 'root/binn2_khmodel/data/repro/models")
     parser.add_argument('--khlayer_name', type=str, default='fkhl3_cifar10_pruned.pty', help='name of the layer')
+    parser.add_argument('--attack_models', choice=['khmodel', 'shlp', 'L2', 'jreg', 'specreg'], help="models to train")
+
 
     args = parser.parse_args()
 
@@ -59,27 +75,53 @@ if __name__ == "__main__":
         os.makedirs(exp_path)
 
     # model filenames
-    khlayer_name = Path(args.khlayer_name)
-    model_info = torch.load(model_path / khlayer_name)
-    state_dict = model_info['model_state_dict']
-    khlayer = FKHL3(state_dict)
-               
-    rp_fname = Path("random_perturbation_results.pkl")
-    fgsm_fname = Path("fgsm_results.pkl")
-    pgd_fname = Path("pgd_results.pkl")
+    khmodel_name = Path("khmodel_cifar10_pruned.pty")
+    shlp_init_name = Path("shlp_init.pty")
+    shlp_name = Path("shlp_cifar10.pty")
+    shlp_l2_name = Path("shlp_l2.pty")
+    shlp_jreg_name = Path("shlp_jreg.pty")
+    shlp_specreg_name = Path("shlp_specreg.pty")
+
+    fn_dict = {
+        'khmodel': (khmodel_name,),
+        'shlp': (shlp_init_name, shlp_name),
+        'L2': (shlp_l2_name,),
+        'jreg': (shlp_jreg_name,),
+        'specreg': (shlp_specreg_name,),
+    }           
+
+    fn_list = [fn for model in args.attack_models for fn in fn_dict[model]]
+
+    BATCH_SIZE = 1000
 
     if torch.cuda.is_available():
         device = torch.device('cuda')
     else:
         device = torch.device('cpu')
 
-    print(f"args data path: {args.data_path}")
     cifar10Test = LpUnitCIFAR10(
     root=args.data_path,
     train=False,
     transform=ToTensor(),
     p=FKHL3.pSet["p"],
     )
+
+    idmodel = IdentityModel()
+
+    TestLoader = DeviceDataLoader(
+                cifar10Test,
+                device=device,
+                batch_size=BATCH_SIZE,
+                num_workers=4,
+                shuffle=False,
+            )
+
+    rp_fname = Path("random_perturbation_results.pkl")
+    fgsm_fname = Path("fgsm_results.pkl")
+    pgd_fname = Path("pgd_results.pkl")
+
+
+    print(f"args data path: {args.data_path}")
     
     ce_loss = torch.nn.CrossEntropyLoss()
     eps = np.logspace(-6, np.log(2.5), num=400)
