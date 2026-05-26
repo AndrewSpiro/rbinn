8f26de862ea537cc5555f0c6d0f3bab31dbe5eec
AndrewSpiro <s4015355@vuw.leidenuniv.nl>
2026-05-26 09:37:32 +0200

    buggy khmodel add

--- create_robustness_dist.py
+++ create_robustness_dist.py
@@ -243,7 +243,22 @@ def load_pt_network(network_name: str, network_path):
         network = PyTorchNetwork(model, (1, 3, 32, 32), network_name)
         return network
     elif network_name == "khmodel":
-        print("load khmodel")
+        print("loading khmodel", flush=True)
+        layer_info = torch.load(network_path + "/fkhl3_cifar10_pruned.pty")
+        print("info loaded", flush=True)
+        layer_state_dict = layer_info['model_state_dict']
+        print("got statedict", flush=True)
+        khlayer = FKHL3(layer_state_dict)
+        print("made khlayer", flush=True)
+        khmodel = KHModel(khlayer, no_classes = 10)
+        print("model made", flush=True)
+        model_info = torch.load(network_path + "/khmodel_cifar10_pruned.pty")
+        print("model info loaded", flush=True)
+        khmodel.load_state_dict(model_info)
+        print("state dict loaded", flush=True)
+        network = PyTorchNetwork(khmodel, (1,3,32,32), "khmodel")
+        print("network made", flush=True)
+        return network
     elif network_name == "eat":
         model, _, _, _ = model_dispatcher("cifar10", "rgbedge", "cifar10", 64, 10)
         state_dict = torch.load(network_path)
@@ -334,7 +349,7 @@ if __name__ == "__main__":
 
     parser.add_argument(
         "model",
-        choices=["pixelreg", "eat", "cnnf", "vonenet","cifar_7_1024","convbig"],
+        choices=["pixelreg", "khmodel", "eat", "cnnf", "vonenet","cifar_7_1024","convbig"],
         help="Model on which to obtain robustness distributions.",
     )
 
