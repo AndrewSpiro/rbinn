02e7c83b73ca710640acd0fa539aded76456d160
AndrewSpiro <s4015355@vuw.leidenuniv.nl>
2026-06-03 19:27:44 +0200

    fix: correct edge map

--- binn3_eat/helper_class.py
+++ binn3_eat/helper_class.py
@@ -4,7 +4,7 @@ import torch
 class AddEdgeMap:
     '''This is a custom class to facilitate use of the Borji model with the robustness distribution script'''
     def __call__(self, img):
-        edge_map = detect_edge_gtsrb(img)
+        edge_map = detect_edge_new_cifar(img)
 
         if not isinstance(edge_map, torch.Tensor):
             edge_map = torch.tensor(edge_map, dtype=torch.float32)
